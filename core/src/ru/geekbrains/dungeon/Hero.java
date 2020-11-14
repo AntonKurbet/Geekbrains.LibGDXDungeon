e22645bab028bb90efea0e46a04e19f9bec7cfbe
AntonKurbet <anton.kurbet@gmail.com>
2020-11-14 19:33:05 +0300

    lesson2

--- core/src/ru/geekbrains/dungeon/Hero.java
+++ core/src/ru/geekbrains/dungeon/Hero.java
@@ -9,23 +9,77 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 public class Hero {
+    private static final float SPEED_Y = 100;
+    private static final float SPEED_X = 100;
+    public static final int CELLS_X = 20;
+    public static final int CELLS_Y = 20;
+    public static final int CELL_SIZE = 40;
+
     private ProjectileController projectileController;
     private Vector2 position;
+    private Vector2 positionCell;
+    private Vector2 targetCell;
+    private Vector2 targetPosition;
     private TextureRegion texture;
+    private Vector2 velocity;
+    private Vector2 velocityCell;
+    private float angle;
 
     public Hero(TextureAtlas atlas, ProjectileController projectileController) {
-        this.position = new Vector2(100, 100);
+        this.positionCell = new Vector2(0, 0);
+        this.position = new Vector2();
+        this.position = position.set(positionCell).scl(CELL_SIZE).add(20,20);
+        this.targetCell = new Vector2();
+        this.targetPosition = new Vector2();
+        this.velocity = new Vector2(0,0);
+        this.velocityCell = new Vector2(0,0);
         this.texture = atlas.findRegion("tank");
         this.projectileController = projectileController;
     }
 
     public void update(float dt) {
         if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
-            projectileController.activate(position.x, position.y, 200, 0);
+            projectileController.activate(position.x + 20, position.y + 20, velocityCell.x * 200, velocityCell.y * 200);
         }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.Q)) {
+            projectileController.setNumShots(projectileController.getNumShots() == 1 ? 2 : 1);
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.W)) {
+            velocity.set(0, SPEED_Y);
+            angle = 0;
+            cellCalc();
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.S)) {
+            velocity.set(0, -SPEED_Y);
+            cellCalc();
+            angle = 180;
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.A)) {
+            velocity.set(-SPEED_X,0);
+            cellCalc();
+            angle = 270;
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.D)) {
+            velocity.set(SPEED_X,0);
+            cellCalc();
+            angle = 90;
+        }
+
+        if ((targetCell.x >= 0) && (targetCell.x <= CELLS_X) && (targetCell.y >= 0) && (targetCell.y <= CELLS_Y)) {
+            if (position.epsilonEquals(targetPosition,1)) {
+                velocity.set(0, 0);
+                positionCell.set(targetCell);
+            } else position.mulAdd(velocity, dt);
+        } else velocity.set(0, 0);
+    }
+
+    private void cellCalc() {
+        velocityCell.set(velocity).nor();
+        targetCell.set(positionCell).add(velocityCell);
+        targetPosition.set(targetCell).scl(CELL_SIZE).add(20,20);
     }
 
     public void render(SpriteBatch batch) {
-        batch.draw(texture, position.x - 20, position.y - 20);
+        batch.draw(texture, position.x, position.y);
     }
 }
--- core/src/ru/geekbrains/dungeon/ProjectileController.java
+++ core/src/ru/geekbrains/dungeon/ProjectileController.java
@@ -7,6 +7,14 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class ProjectileController {
     private static final int MAX_PROJECTILES = 100;
     private Projectile[] items;
+    private int numShots;
+
+    public void setNumShots(int numShots) {
+        this.numShots = numShots;
+    }
+    public int getNumShots() {
+        return numShots;
+    }
 
     public Projectile[] getItems() {
         return items;
@@ -14,6 +22,7 @@ public class ProjectileController {
 
     public ProjectileController(TextureAtlas atlas) {
         this.items = new Projectile[MAX_PROJECTILES];
+        this.numShots = 1;
         TextureRegion region = atlas.findRegion("projectile");
         for (int i = 0; i < items.length; i++) {
             items[i] = new Projectile(region);
@@ -21,10 +30,12 @@ public class ProjectileController {
     }
 
     public void activate(float x, float y, float vx, float vy) {
+        int shot = numShots;
         for (Projectile p : items) {
             if (!p.isActive()) {
-                p.activate(x, y, vx, vy);
-                return;
+                p.activate(x + shot * vx / 10, y + shot * vy / 10, vx, vy);
+                shot--;
+                if (shot == 0) return;
             }
         }
     }
