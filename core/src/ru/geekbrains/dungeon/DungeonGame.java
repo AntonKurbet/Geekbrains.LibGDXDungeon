6290df1c838071ad07c4c14da67c93d09bc8f9d4
AntonKurbet <anton.kurbet@gmail.com>
2020-11-27 09:19:35 +0300

    lesson6

--- core/src/ru/geekbrains/dungeon/DungeonGame.java
+++ core/src/ru/geekbrains/dungeon/DungeonGame.java
@@ -1,58 +1,53 @@
 package ru.geekbrains.dungeon;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import ru.geekbrains.dungeon.game.GameController;
+import ru.geekbrains.dungeon.game.GameMap;
+import ru.geekbrains.dungeon.helpers.Assets;
+import ru.geekbrains.dungeon.screens.ScreenManager;
 
-public class DungeonGame extends ApplicationAdapter {
-    private TextureAtlas atlas;
+public class DungeonGame extends Game {
     private SpriteBatch batch;
-    private ProjectileController projectileController;
-    private GameMap gameMap;
-    private Hero hero;
 
     // Домашнее задание:
-    // 0. Разобраться со структурой кода
-    // 1. Кнопкой Q необходимо переключать режим стрельбы: либо стреляем по одному снаряду,
-    // либо по 2
-    // 2. На векторах сделайте движение в разные стороны (влево, вверх, вниз, вправо),
-    // с запретом на выезд за пределы карты
-    // 3. * Снаряд должен выпускаться в сторону последнего движения
+    // 1. Разобраться с кодом
+    // 2. Каждому персонажу дается количество шагов и атак, в начале хода они генерятся в пределах 1-4
+    // 3. На соответствующие действия персонаж тратит эти очки, если сделать ничего не может/не хочет, то
+    // счетчик его ходов обнуляется (действия шаги/атака заменит turns)
+    // 4. Над головой текущего игрока можно отобразить количество каждого действия S2, A3 (step - 2, attack - 3)
+    // 5. * Монеты высыпаются на пол, и чтобы их забрать, надо на них наступить
+    // 6. * Добавить туман войны
+
+    // Идея механики:
+    // 1. На ваш ход выбатает случайный набор действий (2 шага, 4 атаки, 1 спец действие)
+    // 2. На движение тратится по 1 шагу
+    // 3. На обычную атаку требуется 1 атака, на усиленную 3 атаки
+    // 4. Спец действия тратятся на лечение/приготовление/крафт/...
+    // 5. Можно выбирать оружие, на каждого монстра оружие действует по разному, даже в рамках одного класса
+    // *. Для монстров то же самое
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        atlas = new TextureAtlas("images/game.pack");
-        projectileController = new ProjectileController(atlas);
-        hero = new Hero(atlas, projectileController);
-        gameMap = new GameMap(atlas);
+        ScreenManager.getInstance().init(this, batch);
+        ScreenManager.getInstance().changeScreen(ScreenManager.ScreenType.GAME);
     }
 
     @Override
     public void render() {
-        float dt = Gdx.graphics.getDeltaTime();
-        update(dt);
-
-        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        gameMap.render(batch);
-        hero.render(batch);
-        projectileController.render(batch);
-        batch.end();
-    }
-
-    public void update(float dt) {
-        projectileController.update(dt);
-        hero.update(dt);
+        float dt = Gdx.graphics.getDeltaTime();
+        getScreen().render(dt);
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        atlas.dispose();
     }
 }
